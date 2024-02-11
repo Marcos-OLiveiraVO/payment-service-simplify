c40abba3922f6f83deaddc2559e7f8a4dfbe4f1a
Marcos-Corp <marcos.oliveira-ext@parceirodaconstrucao.com.br>
2024-02-11 02:56:17 -0400

    refactor: adjust some views that was missing

--- src/modules/payment/infra/http/viewModels/transactionViewModel.ts
+++ src/modules/payment/infra/http/viewModels/transactionViewModel.ts
@@ -4,9 +4,11 @@ export class TransactionViewModel {
   static toHttp(entity: Transaction) {
     return {
       id: entity.id,
+      amount: entity.amount,
       description: entity.description,
       paymentMethod: entity.paymentMethod,
       cardOwnerName: entity.cardOwner,
+      cardNumber: entity.cardNumber,
       expirationDate: entity.expirationDate,
       cvv: entity.cvv,
       createdAt: entity.createdAt,
