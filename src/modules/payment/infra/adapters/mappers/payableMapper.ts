ef4573a51b61fa877189c4458f68abd3541c3d26
Marcos-OLiveiraVO <marcosoliveira.rd@gmail.com>
2024-02-13 01:28:57 -0400

    feat: add payableMapper toDomain

--- src/modules/payment/infra/adapters/mappers/payableMapper.ts
+++ src/modules/payment/infra/adapters/mappers/payableMapper.ts
@@ -2,6 +2,20 @@ import { payable as PayableModel } from '@prisma/client';
 import { Payable as PayableEntity } from 'src/modules/payment/application/entities/payable';
 
 export class PayableMapper {
+  static toDomain(entity: PayableModel): PayableEntity {
+    return new PayableEntity(
+      {
+        profileClient: entity.profileClientId,
+        transaction: entity.transactionId,
+        paymentDate: entity.paymentDate,
+        status: entity.status,
+        fee: entity.fee,
+        createdAt: entity.createdAt,
+        updatedAt: entity.updatedAt,
+      },
+      entity.id,
+    );
+  }
   static toDatabase(entity: PayableEntity): PayableModel {
     return {
       id: entity.id,
